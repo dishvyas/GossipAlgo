288178883b2a9ff57163eab2875b791530d289e3
shakthi-stack <shakthisaravanan1@outlook.com>
2023-11-28 11:26:54 -0500

    pushsum added

--- Gossip/Gossip.fsx
+++ Gossip/Gossip.fsx
@@ -36,6 +36,10 @@ type GossipNode =
     | Rumor
     | Spread
     | Exhausted
+    // Push-Sum messages
+    | UpdateSum of float * float
+    | SendSum
+    // | SumConverged
 
 // Define message types for Topology actor
 type Topology =
@@ -47,6 +51,7 @@ type GossipSimulator =
     | InitialiseTopology of string
     | TopoDone
     | NodeExhausted
+    // |SumConverged
 
 // TODO: Add Push-Sum Part
 
@@ -68,12 +73,12 @@ let Topology(mail:Actor<_>)=
                         nList <- nList @ [nodelist.Item(i-1)]
                     if i <> numNodes-1 then
                         nList <- nList @ [nodelist.Item(i+1)]
-                    if algo = "gossip" then
+                    if algo = "gossip" || algo = "push-sum"  then
                         nodelist.Item(i)<!AddTopology(topology,simulatorRef,nList)
                     
             elif topology="full" then
                 for i in 0 .. numNodes-1 do
-                    if algo = "gossip" then
+                    if algo = "gossip" || algo = "push-sum" then
                         nodelist.Item(i)<!AddTopology(topology,simulatorRef,[])
                     
             elif topology="2D" || topology="imp2D" then
@@ -91,7 +96,7 @@ let Topology(mail:Actor<_>)=
                         nList <- nList @ [nodelist.Item(i+n)]
                     if topology = "imp2D" then 
                         nList <- nList @ [nodelist.Item(rnd.Next()%numNodes)]
-                    if algo = "gossip" then
+                    if algo = "gossip" || algo = "push-sum" then
                         nodelist.Item(i)<!AddTopology(topology,simulatorRef,nList)  
                                                                                                      
         | TopologyDone ->  
@@ -120,16 +125,22 @@ let Node(mail:Actor<_>)=
     let mutable nodesExhausted = 0
     let mutable simulatorRef = null
     let id = mail.Self.Path.Name |> int
+    // Push-Sum variables
+    let mutable s = float id  // Initial value of s is the actor's id
+    let mutable w = 1.0       // Initial value of w is 1
+    let mutable lastRatios = [||] // Array to keep track of last three s/w ratios
     let rec loop() =actor{
         let! message = mail.Receive()
         // printfn "Received Message: %A" message
         match message with
         | AddTopology(topology,dref,nodelist)->
+            printfn "Received AddTopology message for topology: %s" topology
             neigbhours<-nodelist
             nTopology<-topology
             simulatorRef<-dref
             mail.Sender()<!TopologyDone
         | Rumor ->  
+            printfn "Received Rumor message"
             if not exhausted then
                 if rumorHeard = 0 then
                     hadRumor <- true
@@ -148,6 +159,7 @@ let Node(mail:Actor<_>)=
                     mail.Self<!Spread
                         
         | Spread ->
+            printfn "Received Spread message"
             if not exhausted then
                 let mutable next=rnd.Next()
                 if topology = "full" then
@@ -171,6 +183,7 @@ let Node(mail:Actor<_>)=
                     mail.Self<!Spread
 
         | Exhausted ->  
+            printfn "Received Exhausted message"
             if not hadRumor then
                 mail.Self<!Rumor
             if not exhausted then
@@ -183,6 +196,34 @@ let Node(mail:Actor<_>)=
                     if nodesExhausted = neigbhours.Length then
                         exhausted <- true
                         simulatorRef<!NodeExhausted
+        // Push-Sum specific cases
+        | UpdateSum(newS, newW) ->
+            printfn "Received UpdateSum message: newS=%f, newW=%f" newS newW
+            s <- s + newS
+            w <- w + newW
+            lastRatios <- Array.append lastRatios [|s / w|]
+            if lastRatios.Length > 3 then
+                lastRatios <- lastRatios.[1..]
+            if lastRatios.Length = 3 && 
+               abs (lastRatios.[0] - lastRatios.[2]) < 1e-10 then
+                simulatorRef<!NodeExhausted
+            else
+                mail.Self<!SendSum
+
+        | SendSum ->
+            printfn "Received SendSum message"
+            let halfS = s / 2.0
+            let halfW = w / 2.0
+            s <- halfS
+            w <- halfW
+            // Send the other half to a random neighbor
+            let mutable next = rnd.Next()
+            if topology = "full" then
+                while next % numNodes = id do
+                    next <- rnd.Next()
+                NodeList.Item(next % numNodes)<!UpdateSum(halfS, halfW)
+            else
+                neigbhours.Item(next % neigbhours.Length)<!UpdateSum(halfS, halfW)
         return! loop()
     }
     loop()
@@ -190,30 +231,38 @@ let Node(mail:Actor<_>)=
 
 
 // Spawn GossipNode actors based on the chosen algorithm
-if algo = "gossip" then
+if algo = "gossip" || algo = "push-sum" then
     NodeList <- [for a in 0 .. numNodes-1 do yield(spawn system (string a) Node)] 
 // TODO: Add Push-Sum part
 
 // Define the Gossip Simulator actor
 let Simulator(mail:Actor<_>)=
-    let mutable spread = 0  
+    let mutable spread = 0 
+    let mutable sumsConverged = 0 
     let rec loop() =actor{
         let! message = mail.Receive()
         //printfn "Received Message: %A" message
         match message with
-        | InitialiseTopology(topology) ->     
+        | InitialiseTopology(topology) ->   
+            printfn "Received InitialiseTopology message for topology: %s" topology  
             topologyRef <! CreateTopology(topology,NodeList)
-        | TopoDone ->  
+        | TopoDone ->
+            printfn "Received TopoDone message"
             if algo = "gossip" then
-                NodeList.Item(rnd.Next()%numNodes)<!Rumor
+                NodeList.Item(rnd.Next() % numNodes)<!Rumor
+                timer.Start()
+            elif algo = "push-sum" then
+                NodeList.Item(rnd.Next() % numNodes)<!SendSum
                 timer.Start()
             // TODO: Add Push-Sum Part
-        | NodeExhausted ->  
+        | NodeExhausted ->
+            printfn "Received NodeExhausted message"  
             spread <- spread + 1
             if spread = numNodes then 
                 mail.Context.System.Terminate() |> ignore
                 printfn "%s,%s,%i,%i" algo topology numNodes timer.ElapsedMilliseconds
-                                
+            if algo = "push-sum" then
+                printfn "%s,%s,%i,%i" algo topology numNodes timer.ElapsedMilliseconds
         return! loop()
     }
     loop()
